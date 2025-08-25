c642e715a74293a8059bd38349630b451ec78a65
Leonid.x <126909762+Leonidos67@users.noreply.github.com>
2025-08-25 23:37:23 +0300

    Update base.layout.tsx

--- client/src/layout/base.layout.tsx
+++ client/src/layout/base.layout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 
 const BaseLayout = () => {
   return (
-    <div className="flex flex-col w-full h-auto">
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="w-full mx-auto h-auto ">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <>
+      <Outlet />
+      <Toaster />
+    </>
   );
 };
 
