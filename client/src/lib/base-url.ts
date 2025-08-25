ebff4c83becfbcea851d819ee4e66fbcaa65778d
Leonid.x <126909762+Leonidos67@users.noreply.github.com>
2025-08-25 13:51:37 +0300

    Update base-url.ts

--- client/src/lib/base-url.ts
+++ client/src/lib/base-url.ts
@@ -1,2 +1,2 @@
-const defaultBaseUrl = "https://ts-backend-1xio.onrender.com/api";
+const defaultBaseUrl = "https://tb-backend-1xio.onrender.com/api";
 export const baseURL = import.meta.env.VITE_API_BASE_URL || defaultBaseUrl;
