7032094ac36f15a6c45a99465946130d0fba72ff
Leonid.x <126909762+Leonidos67@users.noreply.github.com>
2025-08-25 22:06:13 +0300

    Update roleGuard.ts

--- backend/src/utils/roleGuard.ts
+++ backend/src/utils/roleGuard.ts
@@ -1,14 +1,21 @@
-import { PermissionType, Permissions } from "../enums/role.enum";
+import { PermissionType, Permissions, Roles } from "../enums/role.enum";
 import { UnauthorizedException } from "./appError";
 import { RolePermissions } from "./role-permission";
 
 export const roleGuard = (
-  role: keyof typeof RolePermissions,
+  role: keyof typeof Roles | undefined | null,
   requiredPermissions: PermissionType[]
 ) => {
-  const permissions = RolePermissions[role];
-  // If the role doesn't exist or lacks required permissions, throw an exception
+  // Check if role exists and is valid
+  if (!role || !RolePermissions[role]) {
+    throw new UnauthorizedException(
+      "Invalid or missing role. You do not have the necessary permissions to perform this action"
+    );
+  }
 
+  const permissions = RolePermissions[role];
+  
+  // If the role doesn't exist or lacks required permissions, throw an exception
   const hasPermission = requiredPermissions.every((permission) =>
     permissions.includes(permission)
   );
