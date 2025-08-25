2c1d48533ba4edfcd4fa749c64e4ec930e19eeb7
Leonid.x <126909762+Leonidos67@users.noreply.github.com>
2025-08-25 23:37:51 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { NuqsAdapter } from "nuqs/adapters/react";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 import "./index.css";
-import App from "./App.tsx";
-import QueryProvider from "./context/query-provider.tsx";
-import { ThemeProvider } from "./context/theme-provider.tsx";
-import { Toaster } from "./components/ui/toaster.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <QueryProvider>
-        <NuqsAdapter>
-          <App />
-        </NuqsAdapter>
-        <Toaster />
-      </QueryProvider>
-    </ThemeProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
